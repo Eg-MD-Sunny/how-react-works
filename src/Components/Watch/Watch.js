6a661f77e08bef63ee53ebe4e90c6210ef65bf86
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-05 22:13:47 +0600

    Modify

--- src/Components/Watch/Watch.js
+++ src/Components/Watch/Watch.js
@@ -14,7 +14,7 @@ const Watch = () => {
         <div style={{border:"2px solid purple", margin:"20px"}}>
             <h2>This is my smart watch</h2>
             <h3>My Current Steps: {steps}</h3>
-            <button onClick={stepCount}>De Dour............</button>
+            <button onClick={stepCount}>De Dour..........</button>
             <Dispaly name="iPhone13" steps={steps}></Dispaly>
         </div>
     );
