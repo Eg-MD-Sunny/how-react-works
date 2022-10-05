7b670250fdae41af0ab8363dac2363814ef37c81
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-05 22:04:13 +0600

    Modify

--- src/Components/Display/Dispaly.js
+++ src/Components/Display/Dispaly.js
@@ -6,7 +6,7 @@ const Dispaly = (props) => {
         <div style={{border:'2px solid green', margin:'5px', borderRadius:'5px'}}>
            <h2>Name: {props.name}</h2> 
            <p>So far steps today: {props.steps}</p>
-           <Dail></Dail>
+           <Dail steps={props.steps}></Dail>
         </div>
     );
 };
