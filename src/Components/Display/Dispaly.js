e417a371e1ea913e60da689305ce28b0e673c90e
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-05 21:49:29 +0600

    Modify

--- src/Components/Display/Dispaly.js
+++ src/Components/Display/Dispaly.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Dispaly = (props) => {
     return (
-        <div>
+        <div style={{border:'2px solid green', margin:'5px', borderRadius:'5px'}}>
            <h2>Name: {props.name}</h2> 
            <p>So far steps today: {props.steps}</p>
         </div>
