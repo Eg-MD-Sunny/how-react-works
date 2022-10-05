48b71597cd5240bac067653bc1a3e74b388ef5d3
Md Naimur Zaman Sunny <egmdsunny@gmail.com>
2022-10-05 21:59:03 +0600

    Modify

--- src/Components/Dial/Dail.js
+++ src/Components/Dial/Dail.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Dail = () => {
+const Dail = (props) => {
     return (
         <div>
-            
+           <h3>This is Dial</h3> 
+           <p>Steps so far: {props.steps}</p>
         </div>
     );
 };
--- src/Components/Display/Dispaly.js
+++ src/Components/Display/Dispaly.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Dail from '../Dial/Dail';
 
 const Dispaly = (props) => {
     return (
         <div style={{border:'2px solid green', margin:'5px', borderRadius:'5px'}}>
            <h2>Name: {props.name}</h2> 
            <p>So far steps today: {props.steps}</p>
+           <Dail></Dail>
         </div>
     );
 };
